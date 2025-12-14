b2ee0236d4ac28f20fbb6c2ab0e2c39a26332965
ilayadmoni <ilayadmoni9@gmail.com>
2025-12-14 16:55:27 +0200

    working on menu bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/homepage/HomePage";
+import TaskPage from "./pages/taskpage/TaskPage";
+import ChatPage from "./pages/chatpage/ChatPage";
+import LoginPage from "./pages/loginpage/LoginPage";
+import SettingPage from "./pages/settingpage/SettingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Login route - standalone without Layout */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* All other routes wrapped in Layout (with MenuBar and LineBar) */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/tasks" element={<TaskPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/settings" element={<SettingPage />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/homepage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+      <p>Welcome to the HD Manager</p>
+    </div>
+  );
+};
+
+export default HomePage;
